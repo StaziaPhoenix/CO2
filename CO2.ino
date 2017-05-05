99223de3bf3183bfcb8c469a86166686e96e3637
Colin Keef <ckeef@ckeef.local>
2017-05-05 10:54:27 -0700

    Finished Pump class for now.  Commented and cleaned up code

--- CO2.ino
+++ CO2.ino
@@ -1,66 +1,29 @@
 #include "Pump.h"
 
+#define out 1         //Set direction out to be 1
+#define in 0          //Set direction in to be 0
+
 // Serial CMD
 int input;                 //Initialize serial input
-
 
 // Button
 const int startswitch = 9; //Button pin
 
 
-// Valve
-//const int steppin = 3;     //Stepper motor pin
-//const int dirpin = 5;      //Motor direction pin
-//const int valve1 = 11;     //Pin for Valve 1 control (input valve)
-//const int valve2 = 12;     //Pin for Valve 2 control (output valve)
-const int out = 1;         //Set direction out to be 1
-const int in = 0;          //Set direction in to be 0
+
+// Valve & Pump
+Pump pump(3,5,11,12); // Initialize pump object
+//Pump(step motor/valve drive pin,step motor/valve direction pin,valve1/input-valve pin,valve2/output-valve pin);
+
 int stepSize = 0;     
 
-Pump pump(3,5,11,12); // Initialize pump object
-//Pump(step_mtr_drv,step_mtr_dir,valve_input_pin,valve_output_pin);
-
-/*
-void pump(int steps, boolean dir) {
-  if(dir == 1) {
-    digitalWrite(dirpin,HIGH); //Set pump moving outwards with dir=1
-    Serial.println("This pump is moving out");
-  }else if(dir ==0){
-    digitalWrite(dirpin,LOW);  //Set pump moving inwards with dir=0
-    Serial.println("This pump is moving in");
-  }
-  
-  delay(15);
-
-  Serial.println("This pump is moving!");
-  
-  for (int i = 1; i <= steps ;i++){
-    Serial.println(i);
-    digitalWrite(steppin,HIGH);
-    delay(9); // on for 9ms
-    digitalWrite(steppin,LOW);
-    delay(30);  // off for 11ms
-    }
-}
-*/
-
-
 void setup() {
-//  pump.initialize();
-//  pinMode(steppin, OUTPUT);
-//  pinMode(dirpin, OUTPUT);
   pinMode(startswitch, INPUT);
-//  pinMode(valve1, OUTPUT);
-//  pinMode(valve2, OUTPUT);
-//
-//  digitalWrite(steppin, LOW);  //Initialize pump to off
-//  digitalWrite(dirpin, LOW);   //Direction low is pump moving inwards
-//  
+
   Serial.begin(9600);
 
-//  digitalWrite(valve1, LOW);  
-//  digitalWrite(valve2, LOW);   //OPEN BOTH VALVES
-  pump.pump(500, out);              //Drain the pump and plumbing
+  pump.set_valve_dirs(LOW,LOW);  // Open both input valve and output valve
+  pump.pump(500,out);            //Drain the pump and plumbing
 }
 
 
@@ -83,14 +46,11 @@ void loop() {
 
 
     if(digitalRead(startswitch) == HIGH){
-//      digitalWrite(valve1, LOW);
-//      digitalWrite(valve2, HIGH);  //OPEN VALVE 1, CLOSE VALVE 2
-      pump.set_valve_dirs(LOW,HIGH);  // OPEN VALVE 1, CLOSE VALVE 2
+      pump.set_valve_dirs(LOW,HIGH);  // OPEN INPUT VALVE (VALVE 1), CLOSE OUTPUT VALVE (VALVE 2)
       pump.pump(stepSize,in);
       Serial.println("I pumped in");
-//      digitalWrite(valve1, HIGH);
-//      digitalWrite(valve2, LOW);   //CLOSE VALVE 1, OPEN VALVE 2
-      pump.set_valve_dirs(HIGH,LOW);  // OPEN VALVE 1, CLOSE VALVE 2
+      
+      pump.set_valve_dirs(HIGH,LOW);  // CLOSE INPUT VALVE (VALVE 1), OPEN OUTPUT VALVE (VALVE 2)
       pump.pump(stepSize,out);
       Serial.println("I pumped out");
     }
--- Pump.cpp
+++ Pump.cpp
@@ -6,6 +6,14 @@
 
 
 
+/*
+ * Constructor for pump object.  Then initializes the pins and initial pin states
+ * 
+ * @param step_mtr_drv stepper motor driver/actuator/control pin
+ * @param step_mtr_dir stepper motor direction pin
+ * @param valve_input_drv input valve driver/control pin (valve1)
+ * @param valve_output_drv output valve driver/control pin (valve2)
+ */
 Pump::Pump(byte step_mtr_drv,byte step_mtr_dir,byte val_in_pin,byte val_out_pin) {
   
   this->step_mtr_drv=step_mtr_drv;
@@ -16,19 +24,39 @@ Pump::Pump(byte step_mtr_drv,byte step_mtr_dir,byte val_in_pin,byte val_out_pin)
   initialize();
 }
 
+ /*
+ * Sets the input and output valve directions
+ * 
+ * @param input_valve_dir 1 to close, 0 to open input valve
+ * @param output_valve_dir 1 to close, 0 to open output valve
+ */
 void Pump::set_valve_dirs(bool input_valve_dir,bool output_valve_dir) {
   digitalWrite(this->valve_input_drv,input_valve_dir);
   digitalWrite(this->valve_output_drv,output_valve_dir);   
+//  
+//  if(input_valve_dir)
+//    digitalWrite(this->valve_input_drv,HIGH);
+//  else
+//    digitalWrite(this->valve_input_drv,LOW);
+//
+//  if(output_valve_dir)
+//    digitalWrite(this->valve_output_drv,HIGH);   
+//  else
+//    digitalWrite(this->valve_output_drv,LOW);   
 }
 
+/*
+ * Actuates the valves and pump in a direction for a given step size
+ * 
+ * @param step_size how many times to perform a "micro" pump
+ * @param direction in which to pump, 1 for outward, 0 for inward
+ */
 void Pump::pump(int step_size, bool dir) {
   if(dir) {
     step_mtr_set_dir(HIGH); // Set stepper motor (pump) to move outwards (i.e. dir=1)
-//    digitalWrite(dirpin,HIGH); //Set pump moving outwards with dir=1
     Serial.println("This pump is moving out");
   } else if(!dir){
-    step_mtr_set_dir(LOW);
-//    digitalWrite(dirpin,LOW);  // Set stepper motor (pump) to move inwards (i.e. dir=0)
+    step_mtr_set_dir(LOW);  // Set stepper motor (pump) to move inwards (i.e. dir=0)
     Serial.println("This pump is moving in");
   }
   
@@ -39,10 +67,8 @@ void Pump::pump(int step_size, bool dir) {
   for (int i=0;i<step_size;i++){
     Serial.println(i);
     
-//    digitalWrite(steppin,HIGH);
     step_mtr_actuate(HIGH);
     delay(9); // on for 9ms
-//    digitalWrite(steppin,LOW);
     step_mtr_actuate(LOW);
     delay(11);  // off for 11ms or 30ms?
   }
@@ -53,7 +79,9 @@ void Pump::pump(int step_size, bool dir) {
 /**********     PRIVATE FUNCTIONS     **********/
 
 
-
+/*
+ * Initializes the pinout and initial state for stepper motor and valves
+ */
 void Pump::initialize() {
   
   // Set pinout configuration for stepper motor
@@ -65,25 +93,37 @@ void Pump::initialize() {
   pinMode(this->valve_output_drv,OUTPUT);
 
   // Set initial state for stepper motor
-  digitalWrite(this->step_mtr_drv,LOW);  //Initialize pump to off
-  digitalWrite(this->step_mtr_dir,LOW);   //Direction low is pump moving inwards
+  digitalWrite(this->step_mtr_drv,LOW); // Set stepper motor to OFF
+  digitalWrite(this->step_mtr_dir,LOW); // Set stepper motor to be inward directed
 
   // Set initial state for valve
-  digitalWrite(this->valve_input_drv,LOW);  
-  digitalWrite(this->valve_output_drv,LOW);   //OPEN BOTH VALVES  
+  digitalWrite(this->valve_input_drv,LOW);  // OPEN INPUT VALVE (VALVE 2)
+  digitalWrite(this->valve_output_drv,LOW); // OPEN OUTPUT VALVE (VALVE 1)
 }
 
+/*
+ * Sets the stepper motor direction
+ * 
+ * @param dir direction of stepper motor 1 for outward, 0 for inward
+ */
 void Pump::step_mtr_set_dir(bool dir) {
-  if(dir)
-    digitalWrite(this->step_mtr_dir,HIGH);
-  else
-    digitalWrite(this->step_mtr_dir,LOW);
+  digitalWrite(this->step_mtr_dir,dir);
+//  if(dir)
+//    digitalWrite(this->step_mtr_dir,HIGH);
+//  else
+//    digitalWrite(this->step_mtr_dir,LOW);
 }
 
+/*
+ * Actuates the stepper motor
+ * 
+ * @param state 1 for motor ON, 0 for motor OFF
+ */
 void Pump::step_mtr_actuate(bool state) {
-  if(state)
-    digitalWrite(this->step_mtr_drv,HIGH);
-   else
-    digitalWrite(this->step_mtr_drv,LOW);
+  digitalWrite(this->step_mtr_drv,state);
+//  if(state)
+//    digitalWrite(this->step_mtr_drv,HIGH);
+//   else
+//    digitalWrite(this->step_mtr_drv,LOW);
 }
 
